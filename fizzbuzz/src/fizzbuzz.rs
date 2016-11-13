5f86b757ed9a4d04b5c97bc072267d1fdd489b48
Carlos B <carlos.baezruiz@gmail.com>
2016-11-13 23:48:57 +0100

    Added first tests for fizzbuzz

--- fizzbuzz/src/fizzbuzz.rs
+++ fizzbuzz/src/fizzbuzz.rs
@@ -1,5 +1,11 @@
 pub fn fizzbuzz(input: i32) -> String { 
-    let value = "one";
+    let mut value = "1";
+    if input==2 {
+        value = "2";
+    } else if input==3 {
+        value = "fizz";
+    }
+
     value.to_string()
 }
 
@@ -7,10 +13,25 @@ pub fn fizzbuzz(input: i32) -> String {
 
 #[test]
 fn fizz_buzz_when_is_one_then_returns_one() {
-    println!("fizz buzz one");
     let val = fizzbuzz(1);
-    assert_eq!("one",&val);
+    assert_eq!("1",&val);
 }
+#[test]
+fn fizz_buzz_when_is_two_then_returns_two() {
+    let val = fizzbuzz(2);
+    assert_eq!("2",&val);
+}
+#[test]
+fn fizz_buzz_when_is_three_then_returns_fizz() {
+    let val = fizzbuzz(3);
+    assert_eq!("fizz",&val);
+}
+#[test]
+fn fizz_buzz_when_is_four_then_returns_four() {
+    let val = fizzbuzz(4);
+    assert_eq!("4",&val);
+}
+
 
 fn main() {
 }
