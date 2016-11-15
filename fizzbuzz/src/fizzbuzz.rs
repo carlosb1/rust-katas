7d9458978abb4f33b3b0adf43e18180dfda0e02e
Carlos B <carlos.baezruiz@gmail.com>
2016-11-15 23:19:05 +0100

    Finished kata

--- fizzbuzz/src/fizzbuzz.rs
+++ fizzbuzz/src/fizzbuzz.rs
@@ -1,13 +1,14 @@
 pub fn fizzbuzz(input: i32) -> String { 
     let mut value: String = "1".to_string();
 
-    if input !=3 && input !=5 {
+
+    if (input%3)!=0 && (input%5)!=0 {
         value=input.to_string(); 
         return value;
     }
-    if input==3 {
+    if input%3==0 {
         value = "fizz".to_string();
-     } else if input==5 {
+     } else if input%5==0 {
         value = "buzz".to_string();
      }
 
@@ -40,6 +41,22 @@ fn fizz_buzz_when_is_four_then_returns_four() {
 fn fizz_buzz_when_is_five_then_returns_five() {
     let val = fizzbuzz(5);
     assert_eq!("buzz",&val);
+}
+#[test]
+fn fizz_buzz_when_is_divisible_for_three_then_returns_fizz() {
+    let val = fizzbuzz(6);
+    assert_eq!("fizz",&val);
+}
+
+#[test]
+fn fizz_buzz_when_is_divisible_for_five_then_returns_buzz() {
+    let val = fizzbuzz(10);
+    assert_eq!("buzz",&val);
+}
+#[test]
+fn fizz_buzz_when_is_divisible_for_both_then_returns_fizzbuzz() {
+    let val = fizzbuzz(15);
+    assert_eq!("fizzbuzz",&val);
 }
 
 
