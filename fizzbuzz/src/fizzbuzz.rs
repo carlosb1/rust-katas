de217cf0ff500a9c1e0dfca075622218c190e936
Carlos B <carlos.baezruiz@gmail.com>
2016-11-15 23:30:04 +0100

    Refactorized test

--- fizzbuzz/src/fizzbuzz.rs
+++ fizzbuzz/src/fizzbuzz.rs
@@ -19,43 +19,43 @@ pub fn fizzbuzz(input: i32) -> String {
 
 
 #[test]
-fn one_then_returns_one() {
+fn when_one_then_returns_one() {
     let val = fizzbuzz(1);
     assert_eq!("1",&val);
 }
 #[test]
-fn two_then_returns_two() {
+fn when_two_then_returns_two() {
     let val = fizzbuzz(2);
     assert_eq!("2",&val);
 }
 #[test]
-fn three_then_returns_fizz() {
+fn when_three_then_returns_fizz() {
     let val = fizzbuzz(3);
     assert_eq!("fizz",&val);
 }
 #[test]
-fn four_then_returns_four() {
+fn when_four_then_returns_four() {
     let val = fizzbuzz(4);
     assert_eq!("4",&val);
 }
 #[test]
-fn five_then_returns_five() {
+fn when_five_then_returns_five() {
     let val = fizzbuzz(5);
     assert_eq!("buzz",&val);
 }
 #[test]
-fn is_divisible_for_three_then_returns_fizz() {
+fn when_is_divisible_for_three_then_returns_fizz() {
     let val = fizzbuzz(6);
     assert_eq!("fizz",&val);
 }
 
 #[test]
-fn is_divisible_for_five_then_returns_buzz() {
+fn when_is_divisible_for_five_then_returns_buzz() {
     let val = fizzbuzz(10);
     assert_eq!("buzz",&val);
 }
 #[test]
-fn is_divisible_for_both_then_returns_fizzbuzz() {
+fn when_is_divisible_for_both_then_returns_fizzbuzz() {
     let val = fizzbuzz(15);
     assert_eq!("fizzbuzz",&val);
 }
