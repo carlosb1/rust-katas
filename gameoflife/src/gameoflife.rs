446ce99c65313deffcdf619e2d294560a8df3f4a
Carlos B <carlos.baezruiz@gmail.com>
2017-07-08 14:31:17 +0200

    First for gameoflife

--- gameoflife/src/gameoflife.rs
+++ gameoflife/src/gameoflife.rs
@@ -1,5 +1,17 @@
+struct Grid {
+    width: i32,
+    height: i32,
+}
+
+
+impl Grid  {
+    fn cells(&self) -> i32 { return 0}
+}
+
+
 #[test]
-fn first_test() {
+fn grid_should_exit_where_is_initialised() {
+    let grid = Grid{width: 1,height: 1};
+    assert_eq!(grid.cells(),0);
 }
-fn main() {
-}
+
