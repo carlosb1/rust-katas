9c4fdfa072a94b99851f197c10a71c87eef1c3bf
Carlos B <mcflurry0@gmail.com>
2017-10-22 20:43:35 +0200

    Added test for first discount

--- potter/src/lib.rs
+++ potter/src/lib.rs
@@ -16,9 +16,28 @@ impl Potter  {
         self.books.push(new_book);
     }
 
-    pub fn checkout(&self) -> i32  {
+    pub fn checkout(&self) -> f32  {
 
         let number_books = self.books.len() as i32;
-        return (number_books * 8);
+        
+        let mut different = false;
+        for book in &self.books {    
+            for book_to_compare in &self.books {
+                    if book != book_to_compare {
+                        different = true;
+                    }
+            }
+        }
+
+
+        if  different {
+                //TODO move this magic number
+                let sum_books_price = (number_books as f32 * 8.0)  ;
+                //TODO move this percent
+                return sum_books_price * 0.95;
+        }
+        
+        return (number_books as f32 * 8.0);
     }
+
 }
--- potter/tests/tests.rs
+++ potter/tests/tests.rs
@@ -1,37 +1,38 @@
 extern crate potter;
 
 use potter::Potter;
-use std::string::String;
+
 
 #[test]
 fn should_be_created_empty()  {
-    let mut pot = Potter::new();
+    let pot = Potter::new();
     let total = pot.checkout();
 
-    assert_eq!(total,0);
-}
-
-
-#[test]
-fn should_add_one_book()  {
-    let mut pot = Potter::new();
-    let mut book = "book1".to_string();
-    pot.add(book);
-    
-    let total = pot.checkout();
-    assert_eq!(total,8);
+    assert_eq!(total,0.0);
 }
 
 #[test]
 fn should_add_two_same_books()  {
     let mut pot = Potter::new();
-    //TODO entity object!! 
-    let mut book = "book1".to_string();
-    let mut book2 = "book1".to_string();
-    pot.add(book);
-    pot.add(book2);
-   
+    let book = "book1".to_string();
+    //TODO refactor to move this to_string, should it be static?
+    pot.add(book.clone());
+    pot.add(book.clone());
 
     let total = pot.checkout();
-    assert_eq!(total,16);
+    assert_eq!(total,16.0);
 }
+
+#[test]
+fn should_add_different_books()  {
+    let mut pot = Potter::new();
+    let book = "book1".to_string();
+    let book2 = "book2".to_string();
+    pot.add(book.clone());
+    pot.add(book2.clone());
+    
+    let total = pot.checkout();
+    assert_eq!(total,15.2);
+}
+
+
